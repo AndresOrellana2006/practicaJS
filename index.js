127e8f26f4f440c17acfdbba377d392b1c0d297e
Andres Orellana <orellana.leonardoandres@iesconcepcion.edu.ar>
2025-06-12 08:53:07 -0300

    Inicio de practica

---
+++ index.js
@@ -0,0 +1,65 @@
+/*
+const ingresoEdad = prompt("Ingrese su edad");
+ingresoEdad = 1000
+const edadPersona = parseInt(ingresoEdad);
+const edadvoto = parseInt(prompt("¡cuantos años tenes?"));
+
+if (isNaN(edadvoto)) {
+    alert("Por favot, ingrese un numero valido");
+} else if (edadvoto >= 16) {
+    alert("Puedes votar");
+} else {
+    alert("No podes votar");
+}
+*/
+
+//nombre = "andres"
+//apellido = "orellana"
+//nombreCompleto = nombre + apellido;
+//console.log(nombreCompleto)
+
+const edad = parseInt(prompt("Ingrese su edad"))
+if (edad == 19) {
+    console.log("Si es su edad")
+} else {
+    console.log("No es su edad")
+}
+
+let resta = 4 - 8;
+console.log("La resta es: " + resta);
+
+//nombre = "andres"
+//apellido = "orellana"
+//nombreCompleto = nombre + apellido;
+//console.log("su nombre completo es: " + nombreCompleto)
+
+let a = 10;
+console.log(a > 5)
+console.log(a >= 5)
+console.log(a < 5)
+
+
+
+let nombre = "Andres";
+let apellido = " Orellana ";
+console.log("Mi nombre completo es: " + nombre + apellido )
+
+/*
+const a = 10;
+const b = 20;
+const c = 5
+console.log(a * b - c)
+
+const resta = 5 - 2
+console.log("El resultado de la resta es: " +resta)
+*/
+
+
+const voto = parseInt(prompt("Ingresee su edad"))
+if (isNaN(voto)){
+} else if (voto >= 16) {
+    console.log("Puedes votar")
+} else {
+    console.log("No podes votar")
+}
+
